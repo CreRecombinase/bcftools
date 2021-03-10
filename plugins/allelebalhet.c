49438a17fdeba8b3761d84594ee805c3ede4ff0a
Nicholas Knoblauch <knoblauch.nicholas@gene.com>
2021-03-10 23:49:07 +0000

    Changed allelebalhet to work on mutli-allelics

--- plugins/allelebalhet.c
+++ plugins/allelebalhet.c
@@ -22,21 +22,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.  */
 
+#include <htslib/kbitset.h>
+#include <htslib/vcfutils.h>
 #include <getopt.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdbool.h>
+#include "bcftools.h"
 #include <htslib/vcf.h>
 
 typedef struct _args_t
 {
   bcf_hdr_t *hdr;        /*! VCF file header */
-  uint32_t *gt_arr;      /*! temporary array, to store GTs of current line/record */
-  int ngt_arr;           /*! hold the number of current GT array entries */
-  uint32_t *ad_arr;      /*! temporary array, to store ADs of current line/record*/
-  int nad_arr;           /*! hold the number of current AD array entries */
-  float het_ab_thresh;   /*! threshold for heterozygous allele balance filter */
-  uint32_t min_het_covg; /*! minimum coverage of heterozygous sites across samples before applying filter */
+  float het_ab_thresh_snp;   /*! threshold for heterozygous allele balance filter for SNPs */
+  float het_ab_thresh_indel;   /*! threshold for heterozygous allele balance filter for INDELs*/
+  uint32_t min_het_covg_snp; /*! minimum coverage of heterozygous sites across samples before applying filter */
+  uint32_t min_het_covg_indel; /*! minimum coverage of heterozygous sites across samples before applying filter */
+  kbitset_t *rm_als;     /*! bitset for keeping track of which alleles to keep and drop */
 } args_t;
 
 static args_t args;
@@ -60,11 +64,13 @@ const char *usage(void)
         "   run \"bcftools plugin\" for a list of common options\n"
         "\n"
         "Plugin options:\n"
-        "  -m,--mindepth <integer>  min depth of coverage of het sites accross samples\n"
-        "  -a,--maxabal <float>     filter if het site allele balance < maxabal or > ( 1-maxabal ) \n"
+        "  -s,--mindepth_snp <integer>  min depth of coverage for het sites across samples for SNPs\n"
+        "  -i,--mindepth_indel <integer>  min depth of coverage for het sites across samples for INDELs\n"
+        "  -S,--ab_snp <float>   allele balance threshold for SNPs.  A SNP site must have at least one sample with an allele balance between ab_snp and (1-ab_snp) otherwise the site is excluded\n"
+        "  -I,--ab_indel <float>   allele balance threshold for SNPs.  A SNP site must have at least one sample with an allele balance between ab_snp and (1-ab_snp) otherwise the site is excluded\n"
         "\n"
         "Example:\n"
-        "   bcftools plugin allelebalhet in.vcf -- -adcut 0.1 \n"
+        "   bcftools plugin +allelebalhet in.vcf -o out.vcf -- -s 7 -i 10 -S 0.15 -I 0.2 \n"
         "\n";
 }
 
@@ -75,98 +81,110 @@ const char *usage(void)
 */
 int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
 {
-  args.het_ab_thresh = 0.3; 
-  args.min_het_covg = 300;
+  args.het_ab_thresh_snp = 0.15; 
+  args.het_ab_thresh_indel = 0.2;
+  args.min_het_covg_snp = 7;
+  args.min_het_covg_indel = 10;
 
   static struct option loptions[] =  {
-      {"mindepth",1,0,'m'},
-      {"abal",1,0,'a'},      
+      {"mindepth_snp",1,0,'s'},
+      {"mindepth_indel",1,0,'i'},
+      {"ab_snp",1,0,'S'},
+      {"ab_indel",1,0,'I'},      
       {0,0,0,0}
   };
   
   int c;
   while ((c = getopt_long(argc, argv, "m:a:?h",loptions,NULL)) >= 0)    {
     switch (c) {
-    case 'm': args.min_het_covg = atoi(optarg); break;
-    case 'a': args.het_ab_thresh = atof(optarg); break;      
+    case 's': args.min_het_covg_snp = atoi(optarg); break;
+    case 'S': args.het_ab_thresh_snp = atof(optarg); break;
+    case 'i': args.min_het_covg_indel = atoi(optarg); break;
+    case 'I': args.het_ab_thresh_indel = atof(optarg); break;      
     case 'h': case '?':
     default: fprintf(stderr,"%s", usage()); exit(1); break;
     }
   }
-  
+  args.het_ab_thresh_snp = fmax(args.het_ab_thresh_snp,1-args.het_ab_thresh_snp);
+  args.het_ab_thresh_indel = fmax(args.het_ab_thresh_snp,1-args.het_ab_thresh_indel);
   args.hdr = bcf_hdr_dup(in);  
-  args.gt_arr = NULL; args.ngt_arr = 0;
-  args.ad_arr = NULL; args.nad_arr = 0;
     
   return 0; // Print header always. 
 }
 
-
+ bool allele_passes_balance_threshold(const bcf1_t *line,const  bcf_fmt_t *fmt,const int ismpl,const int ial,const int jal,const float balance_threshold, const int64_t min_depth){
+// float depth_ratio;
+  #define BRANCH_INT(type_t,missing,vector_end) {  \
+     const type_t *p = (type_t *) (fmt->p + fmt->size*ismpl); \
+     if ( p[ial]==vector_end || p[jal]==vector_end ) return false; \
+     if ( p[ial]==missing || p[jal]==missing ) return false; \
+     if ( !p[ial] && !p[jal] ) return false; \
+     const type_t total_depth = p[ial]+p[jal]; \
+     if(total_depth< min_depth) return false; \
+     float depth_ratio = (float)p[ial]/(total_depth); \
+     depth_ratio = fmax(depth_ratio,1-depth_ratio); \
+     if ( (depth_ratio > balance_threshold) ) return false; \
+  } /*This is the move we have to do because C doesn't have overloads...*/
+  switch (fmt->type) {
+      case BCF_BT_INT8: BRANCH_INT(int8_t, bcf_int8_missing, bcf_int8_vector_end); break;
+      case BCF_BT_INT16: BRANCH_INT(int16_t, bcf_int16_missing, bcf_int16_vector_end); break;
+      case BCF_BT_INT32: BRANCH_INT(int32_t, bcf_int32_missing, bcf_int32_vector_end); break;
+      default: fprintf(stderr, "[E::%s] todo: %d\n", __func__, fmt->type); exit(1); break;
+  }
+  //fprintf(stderr, "est=%f\t%d\t%d\n", depth_ratio, total_depth,ial);
+  return true;
+  #undef BRANCH_INT
+}
 /*
     Called for each VCF record. Return rec to output the line or NULL
     to suppress output.
 */
 bcf1_t *process(bcf1_t *rec)
 {
-  if ( !rec->n_sample ) return rec; // no samples, skip record. 
+  if ( !rec->n_sample ) return rec; // no samples, skip record.
+
+/* Initialize a full bitset with an element for each allele.
+As hets pass the ratio and depth test, remove them from the bitset.
+Anything that's left after looping over all the samples gets removed
+*/
+  if ( !args.rm_als )
+    args.rm_als = kbs_init(rec->n_allele);                                         
+  else if ( args.rm_als->n_max < rec->n_allele )
+    kbs_resize(&args.rm_als, rec->n_allele);
+  kbs_insert_all(args.rm_als);
   
-  int ngts = bcf_get_genotypes(args.hdr, rec, &args.gt_arr, &args.ngt_arr);
-  if ( ngts<0 ) return rec; // no genotypes, ouput record
 
-  if(ngts / rec->n_sample != 2) return rec; // strange ploidy, just leave untouched for now
+ bcf_fmt_t *gt_fmt_ptr;
+ if ( !(gt_fmt_ptr = bcf_get_fmt(args.hdr,rec,"GT")) )
+   return rec;
+ bcf_fmt_t *ad_fmt_ptr;
+ if ( !(ad_fmt_ptr = bcf_get_fmt(args.hdr,rec,"AD")))
+   return rec;
 
-  int nad = bcf_get_format_int32(args.hdr, rec, "AD", &args.ad_arr, &args.nad_arr);
-  if ( nad<0 ) return rec; // no AD field, output record
 
-  if(ngts != nad) return rec; // number AD values don't match GTs, output record
 
-  const uint32_t n_alts = rec->n_allele-1; // number of alternate sites at the locus
-  if(n_alts==0) // If the site is monoallelic it can't have hets, so just return the site
-    return rec;
-
-  uint32_t adalt_het[n_alts]; // Store the number of alternate allele reads in a C11 VLA (we can switch to stuffing things in the global struct too)
-  memset(adalt_het, 0, n_alts*sizeof(*adalt_het));
   // Scan through GT and AD values for each sample.
-  uint32_t adref_het = 0;
+  bool any_passed = false;
   for (uint32_t i=0; i< rec->n_sample; i++) {
-    uint32_t *gt = args.gt_arr + i*rec->n_allele;
-    uint32_t *ad = args.ad_arr + i*rec->n_allele;
+    int ial, jal=0;
+    int gt = bcf_gt_type(gt_fmt_ptr, i, &ial, &jal);
+    if(!(gt== GT_HET_AA || gt == GT_HET_RA)) continue; // If we aren't het with two different alts (HET_AA) or het with ref and alt (HET_RA) we can move on
+    const bool is_indel = (bcf_get_variant_type(rec,ial) == VCF_INDEL) ||  (bcf_get_variant_type(rec,jal) == VCF_INDEL);
+    const float ab_thresh = is_indel ? args.het_ab_thresh_indel : args.het_ab_thresh_snp;
+    const int32_t min_depth = is_indel ? args.min_het_covg_indel : args.min_het_covg_snp;
 
-    // Are we done? 
-    if ( gt[0] == bcf_int32_vector_end ) break;
-    if(bcf_gt_is_missing(gt[0])) continue;
-    adref_het += ad[0];
-    int allele_ref = bcf_gt_allele(gt[0]);
-    for(int a=0; a<n_alts; a++){
-      const int allele_index = a+i;
-      if ( gt[allele_index] == bcf_int32_vector_end ) break;
-      if(bcf_gt_is_missing(gt[allele_index])) continue;
-
-      int allele_alt = bcf_gt_allele(gt[allele_index]);
-
-      if(allele_ref != allele_alt) {
-      // Found a het site, count reads from the two alleles.
-
-      adalt_het[a] += ad[allele_index];
-    }
+    if(allele_passes_balance_threshold(rec,ad_fmt_ptr,i,ial,jal,ab_thresh,min_depth)){
+      any_passed=true;
+      kbs_delete((args.rm_als),ial);
+      kbs_delete((args.rm_als),jal);
     }
   }
-
-  for(int a=0; a<n_alts; a++){
-  // Total number of reads from het sites must be > 0
-    uint32_t total_het = adref_het + adalt_het[a];
-    if(total_het > 0) {
-      float est = (float)adref_het / total_het;
-    int failed_flag = 0;
-    fprintf(stderr, "est=%f %d\n", est, total_het);
-
-    // TOOD: This is only a good estimate if there are a minimum number of total reads.
-    if(est > 0.7 || est < 0.3) failed_flag = 1;
-  
-    if(failed_flag) return NULL; else return rec;
-  } else {
-    return rec;
-  }
+  if(!any_passed) return NULL;
+  bcf_unpack(rec,BCF_UN_ALL);
+  if ( bcf_remove_allele_set(args.hdr, rec, args.rm_als)!=0 ) // Drop the problematic allele(s) from the row and return it
+      error("Failed to subset alleles\n");
+  // TODO: This is only a good estimate if there are a minimum number of total reads.
+  return rec;
 }
 
 
@@ -176,8 +194,7 @@ bcf1_t *process(bcf1_t *rec)
 void destroy(void)
 {
     bcf_hdr_destroy(args.hdr);
-    free(args.gt_arr);
-    free(args.ad_arr);
+    kbs_destroy(args.rm_als);
 }
 
 
